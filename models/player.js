4d48d5b6bcb989c7258825499a72d1c68e5b2a22
mogjam <36071179+mogjam@users.noreply.github.com>
2018-07-13 16:33:27 +0100

    Revert "First Commit"
    This reverts commit 91ff0d7cf020b84743d9f0023f519e6d14d931be.

--- models/player.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-let PlayerSchema = new Schema({
-    name: {type: String, required: true, max: 30},
-});
-
-// Export the model
-module.exports = mongoose.model('Player', PlayerSchema);
